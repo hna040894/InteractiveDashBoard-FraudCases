77f4c4af9bf90af96c9d49cecd3b8d7f2f173b24
Anh Ha <hna040894@gmail.com>
2022-03-25 19:55:00 +0200

    Fix DataTable

--- appMain (1).R
+++ appMain (1).R
@@ -4,37 +4,30 @@ library(readxl)
 library(janitor)
 library(DT)
 
-sheet1 <- read_xlsx('TestCase.xlsx',sheet = 'Sheet1')
+df <- read_xlsx('TestCase.xlsx',sheet = 'Sheet1')
 
-sheet1$Month_Yr <- format(as.Date(sheet1$Date), "%Y-%m")
+df$Month_Yr <- format(as.Date(sheet1$Date), "%Y-%m")
 
-sheet1$Date <- format(sheet1$Date, "%Y-%m-%d") #Created this to format the dates
+df$Date <- format(sheet1$Date, "%Y-%m-%d") #Created this to format the dates
 
-sheet1$Time <- format(sheet1$Time, "%H:%M") #Created this to format the time.
+df$Time <- format(sheet1$Time, "%H:%M") #Created this to format the time.
 
-sheet1$Time2 <- sheet1$Time
+df <- df %>% separate(Time2, c("Hour", "Minutes"), sep = ":")
 
-sheet1 <- sheet1 %>% separate(Time2, c("Hour", "Minutes"), sep = ":")
-
-sheet1 <- sheet1 %>% 
-    clean_names()
-
-#sheet1$hour <- format(sheet1$time, "%H:00")
 
 # Define UI for application
 ui <- navbarPage(
     "Application",
     tabPanel("General",
              sidebarLayout( #All outputs are reactive.
-                 sidebarPanel(uiOutput("rowsSelected"), #Display for the number of rows selected
-                              uiOutput("sum"), #Display for the sum of cases (cost) for rows selected
-                              uiOutput("percentage"), #Display for the % of TTP vs FP (number of rows selected/ total number of rows) *100
-                              tableOutput("result") #Display for the table for the number of cases per year-month
+                 sidebarPanel(uiOutput("rowsSelected"), 
+                              uiOutput("sum"), 
+                              uiOutput("percentage"), 
+                              tableOutput("result")
                               ),
-                 mainPanel(DT::dataTableOutput("datatable"), #Display for the data table
-                           #Using DT:: above forces R to use the dataTable funtions from the DT package. This reduces the chances of errors if the 
-                           #application is scaled and a package with the same function name is used in this code
-                           plotOutput("timePlot")) #Plot display
+                 mainPanel(DT::dataTableOutput("datatable"), 
+                           
+                           plotOutput("timePlot")) 
              )
     )
 )
@@ -43,16 +36,12 @@ ui <- navbarPage(
 server <- function(input, output) {
     dataFile <- sheet1
     
-    #The functions below collects the selected rows from the UI and also subsets them from the data file.
-    #This is set in a reactive block so that it's updated and everything on the page is updated accordingly 
-    #when there is a selection.
     filtered_table <- reactive({
         req(input$datatable_rows_selected)
         dataFile[input$datatable_rows_selected, ]  
     })
     
-    #The below is the function that renders the data table.
-    #Added the filter top so you have the ability to select a particular time or patient id.
+
     output$datatable <- DT::renderDataTable({
         datatable(dataFile, 
                   filter = 'top', 
@@ -61,15 +50,14 @@ server <- function(input, output) {
                   )
         })
     
-    #Function for creating the frequency plot for the time.
+
     output$timePlot <- renderPlot({
         ggplot(filtered_table(), aes(hour)) +
             geom_bar() 
-        #If you want a unique title and labels for the x and y, update the ggplot line
+      
     })
     
-    #The next 3 functions are the functions for the number of rows selected, percentage and total cost.
-    #All the functions are reactive and update when a row is selected. 
+   
     output$rowsSelected <- renderUI({
         numericInput("rowsSelected", "Rows selected",
                      value = length(input$datatable_rows_selected),
@@ -89,9 +77,7 @@ server <- function(input, output) {
         
     })
     
-    #Function for rendering the table output.
     
-    #It counts the number of cases in the selected rows for each year-month combination.  
     output$result <- renderTable({
         #Uses the reative table so that the 
         x <- filtered_table() %>% count(month_yr, sort = TRUE)
